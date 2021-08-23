fad821b357c0f4f123c3b6ec88869d8df964294d
Natalie Natalino <70120038+himoutolife@users.noreply.github.com>
2021-08-23 11:17:16 -0700

    Update EditDeck.js
    updated to include new component deckform

--- src/Layout/EditDeck.js
+++ src/Layout/EditDeck.js
@@ -7,87 +7,67 @@
 //can edit and update form
 //cancel button takes to deck screen
 
-import React, { useState,useEffect } from "react";
+import React, { useState,useEffect }  from 'react'
 import {Link, useHistory,useParams} from "react-router-dom";
 import {readDeck, updateDeck} from "../utils/api/index";
+import DeckForm  from "./DeckForm"
+// allows the user to modify information on an existing deck
+// path: /decks/:deckId/edit
 
 function EditDeck(){
-    const [deck, editDeck] =useState({name:"", description:""})
+    const [deck, setDeck] =useState({id: 0, name:"", description:""})
     const history =useHistory()
     const {deckId} =useParams()
 
-    useEffect(()=>{
+    useEffect(()=>{                                 //fetch card data with useEffect
         const abortController = new AbortController()
         const loadDeck =async () => {
             const response =await readDeck(deckId, abortController.signal)
-            editDeck(() => response)
+            setDeck(() => response)
         }
         loadDeck()
         return () => abortController.abort()
     }, [deckId])
     
-    const handleChange = ({event}) => {
-        editDeck({...deck, [event.target.name]: event.target.value})
-    }
+    // const handleChange = ({event}) => {
+    //     editDeck({...deck, [event.target.name]: event.target.value})
+    // }
     
     const handleSubmit =async(event) => {
         event.preventDefault()
         const response = await updateDeck(deck)
         history.push (`/decks.${response.id}`)
     }
+
+    function changeName(e) {                             //change deck state when name changes
+        setDeck({ ...deck, name: e.target.value });
+    }
+    
+    function changeDesc(e) {                            //change deck state when description changes
+        setDeck({ ...deck, description: e.target.value });
+    }
+
+
         return (
             <div className="col-9 mx-auto">
-                {/*Navigation Bar */}
-                <nav>
+                <nav>                                {/*Navigation Bar */}
                     <ol className="breadcrumb">
                         <li className="breadcrumb-item">
-                            {/*link that redirects back to the home page*/}
-                            <Link to={"/"}>Home</Link>
+                            <Link to={"/"}>Home</Link>       {/*link that redirects back to the home page*/}
                         </li>
-                        {/*let users know that they are on the current page*/}
-                        <li className="breadcrumb-item">
-                            <Link to={`/decks/${deckId}`}>{deck.name}</Link>
-                        </li>
-                        <li className="breadcrumb-item">Edit Deck</li>
+                        <li className="breadcrumb-item">Edit Deck            {/*let users know that they are the "edit deck" page*/}
+                        </li>         
                     </ol>
                 </nav>
-                {/*form for creating a new deck*/}
-                <div className="row pl-3 pb-2">
-                    <h1>Edit Deck</h1>
-                </div>
-                <form onSubmit={handleSubmit}>
-                    <div class="form-group">
-                        <label htmlFor="name">Name</label>
-                        <input 
-                        type="name"
-                        name="name"
-                        value={deck.name}
-                        onChange={handleChange}
-                        id="name" 
-                        className="form-control" 
-                        placeholder={deck.name}
-                        />
-                    </div>
-                    <div class="form-group">
-                        <label for="description">Description</label>
-                        <textarea 
-                        name="description"
-                        value={deck.description}
-                        className="form-control" 
-                        id="description"
-                        placeholder={deck.description} 
-                        rows="4">
-                        </textarea>
-                    </div>
-                    <Link to={`/decks/${deckId}`} name="cancel" className="btn btn-secondary mr-3">Cancel</Link>
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                </form>
+                <h4>Edit Deck</h4>
+                    <DeckForm
+                        submitFunction={handleSubmit}
+                        deck={deck}
+                        changeName={changeName}
+                        changeDesc={changeDesc}
+                    />
             </div>
         )
 }
-
-
-
-
 
 export default EditDeck;
