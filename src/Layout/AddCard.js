0544bb21b359b485a6e521c2a3a275b55bfe871f
Natalie Natalino <70120038+himoutolife@users.noreply.github.com>
2021-08-23 11:22:50 -0700

    Update AddCard.js
    updated jsx

--- src/Layout/AddCard.js
+++ src/Layout/AddCard.js
@@ -6,79 +6,77 @@
 //form with front and back text fields, use <textarea>
 //clicking "save" button has a new card added and connected to relevant deck, form is cleared and process repeated
 //click "done" button takes to decks screen
+// allows the user to add a new card to an existing deck
 
-import React, {useEffect, useState} from "react";
+
+import React, {useEffect, useState} from 'react'
 import { Link, useParams } from "react-router-dom"
 import {createCard, readDeck } from "../utils/api/index";
 import CardForm from "./CardForm";
 
- function AddCard(){
-     const [deck, setDeck] =useState([])
-     const [card, addCard] =useState( {front:"", back:"", deckId:""})//sets the defaults
-     const {deckId} =useParams()
 
-     useEffect(() => {
 
-        const deckInfo = async () => {
-            const response = await readDeck(deckId)
-            setDeck(() => response)
-        }
-        deckInfo()
-    }, [deckId])//stop rendering
+function AddCard(){
+    const [deck, setDeck] =useState([])
+    const [card, setCard] =useState( {front:"", back:"", deckId:""})            //initial form state
+    const {deckId} =useParams()
 
-     const handleChange=(event)=>{
-        addCard({...card,[event.target.name]:event.target.value})
-     }
+    useEffect(() => {                                   //load card from API to determine new card ID
+       const deckInfo = async () => {
+           const response = await readDeck(deckId)      //gets name from current deck
+           setDeck(() => response)
+       }
+       deckInfo()
+   }, [deckId])
 
-     const handleSubmit= async (event) =>{
-        event.preventDefault()
-        addCard({...card, deckId:deckId})
-        await createCard(deckId,card);
-        addCard({front: "", back: "", deckId: ""})
+
+    const handleSubmit= async (event) =>{
+       event.preventDefault()
+       setCard({...card, deckId:deckId})
+       await createCard(deckId,card);
+       setCard({front: "", back: "", deckId: ""})
+   }
+
+
+    function changeFront(e) {                               //updates the state as card info changes
+        setCard({ ...card, front: e.target.value });        //update the state of the card, editing the front value 
+    }
+    function changeBack(e) {
+        setCard({ ...card, back: e.target.value });           //update the state of the card, editing the back value 
     }
 
-    return (
-        <div className="col-9 mx-auto">
-           
-            {/*navigation bar */}
-            <nav aria-label="breadcrumb">
-                <ol className="breadcrumb">
 
-                    {/* a link to the home page */}
-                    <li className="breadcrumb-item">
-                        <Link to="/">
-                            Home
-                        </Link>
+   return (
+       <div className="col-9 mx-auto">
+           <nav aria-label="breadcrumb">                 {/*navigation bar */}
+               <ol className="breadcrumb">
+                   <li className="breadcrumb-item">           {/*link to the home page */}
+                       <Link to="/">
+                           Home
+                       </Link>
+                   </li>
+
+                   <li className="breadcrumb-item">
+                       <Link to={`/decks/${deckId}`}>
+                           {deck.name}
+                       </Link>
+                   </li>
+
+                   <li className="breadcrumb-item">         {/* a link for adding a card */}
+                       Add Card
                     </li>
+               </ol>
+           </nav>                           {/* a heading that display's the deck's name and "add card" */}
 
-                    <li className="breadcrumb-item">
-                        <Link to={`/decks/${deckId}`}>
-                            {deck.name}
-                        </Link>
-                    </li>
-
-                    {/* a link for adding a card */}
-                    <li className="breadcrumb-item">
-                        Add Card
-                        </li>
-
-                </ol>
-            </nav>
-
-
-            <div className="row pl-3 pb-2">
-                {/* a heading that display's the deck's name and "add card" */}
-                <h1>{deck.name}: Add Card</h1>
-            </div>
-            <CardForm 
-            submitForm={handleSubmit} 
-            changeForm={handleChange} 
-            card={card} 
-            deckId={deckId} 
+           <h4>{deck.name}: Add Card</h4>
+            <CardForm
+                submitHandler={handleSubmit}
+                card={card}
+                changeFront={changeFront}
+                changeBack={changeBack}
             />
+            </div>
+   )
 
-        </div>
-    )
-
- }
+}
 export default AddCard;
