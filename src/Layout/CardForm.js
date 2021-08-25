736e101fa70050f2872fa00e8f8efc06a6f8690a
Natalie Natalino <70120038+himoutolife@users.noreply.github.com>
2021-08-25 14:45:38 -0700

    Update CardForm.js

--- src/Layout/CardForm.js
+++ src/Layout/CardForm.js
@@ -1,47 +1,59 @@
-//DONE
-import React from "react";
-import {Link} from "react-router-dom";
+import React from 'react'
+import { useHistory } from "react-router-dom"
 
-function CardForm({handleSubmit, handleChange, card, deckId}){
+function CardForm({ submitHandler, card = {}, changeFront, changeBack}) {
+    const history = useHistory();
+
+    function cardFront() {                  //if there is no card front, display nothing
+      return card.front ? card.front : "";
+    }
+    
+    function cardBack() {                   //if there is no card back, display nothing
+      return card.back ? card.back : "";
+    }
+  
     return (
-        <form id="cardForm" 
-        onSubmit={handleSubmit}>
-            <div>
-                <label>Front</label>
-                <textarea
-                name="front"
-                value={card.front}
-                onChange ={handleChange}
-                id="front"
-                className="form-control"
-                placeholder="Front side of card"
-                rows="4"
-                />
-            </div>
-            <div>
-                <label>Back</label>
-                <textarea
-                name="back"
-                value={card.back}
-                onChange={handleChange}
-                className="form-control"
-                id="back"
-                placeholder="Back side of card"
-                rows="4"
-                />
-            </div>
-            <Link
-            to={`/decks/${deckId}`}
-            name="cancel"
-            className="btn btn-secondary mr-3">
-                Done
-            </Link>
-            <button
-            type="submit"
-            className="btn btn-primary"
-            >Submit</button>
-        </form>
-    )
-}
+      <form id="cardForm">
+        <div className="form-group">
+          <label>Front</label>
+          <textarea
+            className="form-control"
+            id="front"
+            rows="3"
+            value={cardFront()}
+            onChange={changeFront}
+            placeholder="Front side of card"
+          ></textarea>
+        </div>
+        <div className="form-group">
+          <label>Back</label>
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+            value={cardBack()}
+            onChange={changeBack}
+            placeholder="Back side of card"
+          ></textarea>
+        </div>
+        <button
+          className="btn btn-secondary ml-2"
+          type="button"
+          onClick={() => history.go(-1)}
+        >
+          Done
+        </button>
+        <button
+          className="btn btn-primary ml-2"
+          type="submit"
+          onClick={submitHandler}
+        >
+          Save
+        </button>
+      </form>
+    );
+  }
 
-export default CardForm;
+  export default CardForm
+//since multiple components return(display) the same card form, it makes sense to make a seperate component that we can call on later.
+//h1
